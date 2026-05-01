0788bc53f26e4b28e9aa1d8a82f32c7774792246
rohanrony <rohanrony@gmail.com>
2026-05-01 16:44:41 -0700

    Initial Commit

---
+++ notchlet/notchletApp.swift
@@ -0,0 +1,17 @@
+//
+//  notchletApp.swift
+//  notchlet
+//
+//  Created by Rohan Roy on 5/1/26.
+//
+
+import SwiftUI
+
+@main
+struct notchletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
